121fd6e58b2b88bd24eafc820380f1f460027cfb
Garvan Doyle <garvandoyle@gmail.com>
2019-06-02 02:27:33 -0400

    final end node added

--- Server/routes/example.js
+++ Server/routes/example.js
@@ -14,7 +14,7 @@ MongoClient.connect("mongodb://localhost:27017/",function(err,client){
 router.get("/getListName/", async (req, res) => {
   const name = req.params.name;
   console.log(db);
-  let wee = db.collection('garvV1').find({ "Year": {$eq: 2017}}).project({'Company Name':1, '_id':0}).toArray(function(err, documents) {
+  let wee = db.collection('garvV1').find({ 'Year': {$eq: 2017}}).project({'Company Name':1, '_id':0}).toArray(function(err, documents) {
     if (err) throw error;
     res.send(documents);
 });
@@ -23,14 +23,23 @@ router.get("/getListName/", async (req, res) => {
 router.get("/getFinancialData/:name", async (req, res) => {
   const name = req.params.name;
   console.log(db);
-  let wee = db.collection('businesses').find({ "Company Name": name}).toArray(function(err, documents) {
+  let wee = db.collection('garvV1').find({'Company Name':name}).toArray(function(err, documents) {
     if (err) throw error;
     carbonE = {};
     documents.forEach (function(element){
       carbonE[element['Year']] = element['CarbonE Emissions'];
     }); 
-  
+    console.log(carbonE);
     res.send(carbonE);
+});
+});
+
+router.get("/getCompanyData/:name", async (req, res) => {
+  const name = req.params.name;
+  console.log(db);
+  let wee = db.collection('garvV1').find({'Company Name': name}).project({'_id':0,'Ticker':1,'Number of Employees':1,'CDP Climate Change Grade':1,'Company Name':1,'Score':1 }).toArray(function(err, documents) {
+    if (err) throw error;
+    res.send(documents[0]);
 });
 });
 
